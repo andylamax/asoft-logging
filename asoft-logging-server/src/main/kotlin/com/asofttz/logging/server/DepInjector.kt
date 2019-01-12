d00f44746c08085f2a50a7dbbea99d1e7a5c83d7
andylamax <andylamax@programmer.net>
2019-01-12 22:48:10 +0300

    Added credentials files

--- asoft-logging-server/src/main/kotlin/com/asofttz/logging/server/DepInjector.kt
+++ asoft-logging-server/src/main/kotlin/com/asofttz/logging/server/DepInjector.kt
@@ -4,18 +4,34 @@ import com.asofttz.logging.data.dao.ServerLogDao
 import com.asofttz.logging.data.db.LogDataSourceConfig
 import com.asofttz.logging.data.repo.LogRepo
 import com.asofttz.logging.data.viewmodal.LogViewModal
+import kotlinx.coroutines.runBlocking
+import java.io.File
 
 object injection {
 
+    init {
+        val classLoader = javaClass.classLoader
+        val file = File(classLoader.getResource("credentials.txt").file)
+        runBlocking {
+            val line = file.readLines()
+            config.apply {
+                username = line[0].split(" ")[0]
+                password = line[0].split(" ")[1]
+            }
+        }
+    }
+
+    private val credentials = File("")
     private val config = LogDataSourceConfig().apply {
         url = "bolt://localhost:8082"
-        username = "andylamax"
-        password = "andymamson"
+        username = "admin"
+        password = "admin"
     }
 
     private val logDao = ServerLogDao.getInstance(config)
 
     private val logRepo = LogRepo.getInstance(logDao)
 
-    val viewModal = LogViewModal(logRepo)
+    val viewModal: LogViewModal
+        get() = LogViewModal(logRepo)
 }
